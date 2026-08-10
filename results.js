7e35c17ff8b96578868e77acb6deb4ce24beeef8
kastiz-ad <kookied88@gmail.com>
2026-08-11 08:23:38 +0900

    fix: refine medical appointment demo UI

--- results.js
+++ results.js
@@ -1,2 +1,2 @@
-import "./js/pages/results-page.js?v=20260811-medical-foundation-v1";
+import "./js/pages/results-page.js?v=20260811-medical-ui-v2";
 // compatibility lineage: 20260730-investor-demo-mode 20260730-universal-execution
