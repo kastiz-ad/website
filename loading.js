2aa50a1b6f8704004338bbe7719ab01b7a37251f
kastiz-ad <kookied88@gmail.com>
2026-08-12 07:58:24 +0900

    Align approval ONE lockup

--- loading.js
+++ loading.js
@@ -1,1 +1,1 @@
-import "./js/pages/loading-page.js?v=20260812-one-button-alignment-v65";
+import "./js/pages/loading-page.js?v=20260812-one-lockup-baseline-v66";
--- results.js
+++ results.js
@@ -1,3 +1,3 @@
-import "./js/pages/results-page.js?v=20260812-one-button-alignment-v65";
+import "./js/pages/results-page.js?v=20260812-one-lockup-baseline-v66";
 // compatibility lineage: 20260730-investor-demo-mode 20260730-universal-execution
 
