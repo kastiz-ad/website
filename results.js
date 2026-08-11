fb1170ef16d382ff0ec370574b6d7a9bb8a1fb4b
kastiz-ad <kookied88@gmail.com>
2026-08-12 00:21:50 +0900

    Fix medical results two-column desktop grid

--- results.js
+++ results.js
@@ -1,2 +1,2 @@
-import "./js/pages/results-page.js?v=20260811-mission-specific-ui-v4";
+import "./js/pages/results-page.js?v=20260812-medical-two-column-v5";
 // compatibility lineage: 20260730-investor-demo-mode 20260730-universal-execution
