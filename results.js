605964943dd954a995aef0a8d926b8056de7f15a
kastiz-ad <kookied88@gmail.com>
2026-08-12 00:31:07 +0900

    Keep medical grid two-up in preview pane

--- results.js
+++ results.js
@@ -1,2 +1,2 @@
-import "./js/pages/results-page.js?v=20260812-medical-grid-v6";
+import "./js/pages/results-page.js?v=20260812-medical-pane-grid-v7";
 // compatibility lineage: 20260730-investor-demo-mode 20260730-universal-execution
