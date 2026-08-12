5a3009897b9719aee5b6788ff2a6fab7c32cb13b
kastiz-ad <kookied88@gmail.com>
2026-08-13 01:02:26 +0900

    Keep Liberty face and schedule action visible

--- loading.js
+++ loading.js
@@ -1,1 +1,1 @@
-import "./js/pages/loading-page.js?v=20260813-liberty-crop-v76";
+import "./js/pages/loading-page.js?v=20260813-liberty-popup-v77";
--- results.js
+++ results.js
@@ -1,3 +1,3 @@
-import "./js/pages/results-page.js?v=20260813-liberty-crop-v76";
-// compatibility lineage: 20260813-liberty-crop-v76 20260730-universal-execution
+import "./js/pages/results-page.js?v=20260813-liberty-popup-v77";
+// compatibility lineage: 20260813-liberty-popup-v77 20260730-universal-execution
 
